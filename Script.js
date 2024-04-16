8eaac3602f411c21616f00580ffa264603ef722c
Mininugge <165042185+Mininugge@users.noreply.github.com>
2024-04-16 15:27:00 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,19 @@
+var navLinks = document.getElementById("navLinks");
+
+function showMenu(){
+  console.log("1")
+  navLinks.style.right = "0";
+}
+function hideMenu(){
+  console.log("2")
+  navLinks.style.right = "-200px";
+}
+
+
+function showthediv(var1) {
+  console.log(var1)
+  document.getElementsByClassName('show')[0].className='hide';
+  document.getElementById(var1).className="show";
+}
+
 
